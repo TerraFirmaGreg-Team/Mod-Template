81541cdd97b7b82bfadedaae26ddcee5277ac431
Xikaro <os.valerievich@yandex.ru>
2026-09-17 13:58:05 +0500

    init

---
+++ src/main/java/team/terrafirmagreg/mod/ModTemplate.java
@@ -0,0 +1,9 @@
+package team.terrafirmagreg.mod;
+
+public class ModTemplate {
+    public static final String MOD_ID = "mod";
+
+    public static void init() {
+        System.out.println("TestMod initialized");
+    }
+}
